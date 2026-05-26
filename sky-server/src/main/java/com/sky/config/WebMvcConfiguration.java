2841b13d3c09bedbadccc65f8b7c86ac66176354
firefly999 <321679429@qq.com>
2026-05-26 14:07:41 +0800

    fix: switch WebMvcConfiguration to WebMvcConfigurer for Knife4j compatibility

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -9,20 +9,19 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 配置类，注册web层相关组件
  */
 @Configuration
 @Slf4j
-public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Autowired
     private JwtTokenAdminInterceptor jwtTokenAdminInterceptor;
@@ -35,7 +34,8 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      *
      * @param registry
      */
-    protected void addInterceptors(InterceptorRegistry registry) {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
         registry.addInterceptor(jwtTokenAdminInterceptor)
                 .addPathPatterns("/admin/**")
@@ -48,33 +48,12 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     }
 
     /**
-     * 通过knife4j生成接口文档
-     * 
-     * @return
-     */
-    // @Bean
-    // Docket docket() {
-    // log.info("开始生成接口文档...");
-    // ApiInfo apiInfo = new ApiInfoBuilder()
-    // .title("苍穹外卖项目接口文档")
-    // .version("2.0")
-    // .description("苍穹外卖项目接口文档")
-    // .build();
-    // Docket docket = new Docket(DocumentationType.SWAGGER_2)
-    // .apiInfo(apiInfo)
-    // .select()
-    // .apis(RequestHandlerSelectors.basePackage("com.sky.controller"))
-    // .paths(PathSelectors.any())
-    // .build();
-    // return docket;
-    // }
-
-    /**
      * 设置静态资源映射
-     * 
+     *
      * @param registry
      */
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始设置静态资源映射...");
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
@@ -82,11 +61,11 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     /**
      * 添加自定义消息转换器
-     * 
+     *
      * @param converters
      */
     @Override
-    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 创建一个消息转换器对象
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         // 设置消息转换器的支持类型
